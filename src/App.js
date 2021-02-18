1c2efd38d5d3d9f235948eea71ac4f31b02e76c9
Mike Leonard <mtleonardjr@gmail.com>
2021-02-18 12:25:17 -0500

    created responsive template with grid and flexbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="one">1</div>
+      <div className="flex-container">
+        <div className="two">2</div>
+        <div className="three">3</div>
+        <div className="four">4</div>
+      </div>
+      <div className="five">5</div>
     </div>
   );
 }
