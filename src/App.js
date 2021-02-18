00384812e308089359229ec6564a6078705de371
Mike Leonard <mtleonardjr@gmail.com>
2021-02-18 14:03:02 -0500

    componetized the 'HolyGrail' template

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-
-import './App.css';
+import HolyGrail from './components/HolyGrail'
 
 function App() {
   return (
-    <div className="container">
-      <div className="one">1</div>
-      <div className="flex-container">
-        <div className="two">2</div>
-        <div className="three">3</div>
-        <div className="four">4</div>
-      </div>
-      <div className="five">5</div>
+    <div>
+      Hello World
     </div>
+
+    // A five box responsive web outline made with css grid and flexbox
+    //<HolyGrail/>
   );
 }
 
